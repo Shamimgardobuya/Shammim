a3fd5f7be643bc536c7fd06389c465e2d40b10ae
Shamimgardobuya <shamimobuya@gmail.com>
2022-03-21 17:26:12 +0300

    python operators

---
+++ Some.py
@@ -0,0 +1,15 @@
+>>> X= 5
+>>> print(x)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'x' is not defined
+>>> x = 5
+>>> print(x)
+5
+>>> x = 7
+>>> y = 8
+>>> print(x + y)
+15
+>>> 
+[2]+  Stopped                 python3
+
